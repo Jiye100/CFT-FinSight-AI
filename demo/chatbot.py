8492e1c7f02a01798bd108f028d4f381b8390927
Jiye100 <22jiye.baek@gmail.com>
2024-11-08 08:57:30 -0500

    Connect to database

--- demo/chatbot.py
+++ demo/chatbot.py
@@ -3,7 +3,7 @@ import os
 import datascraping
 
 # Get Gemini API key
-os.environ["GEMINI_API_KEY"] = 'YOUR_API_KEY'
+os.environ["GEMINI_API_KEY"] = 'GEMINI_API_KEY' # Put your personal API key
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel('gemini-pro')
 
@@ -16,6 +16,7 @@ prompt = input("What can I help with?\n")
 response = chat.send_message(prompt, stream=True)
 # TODO: get list of keywords from the response
 
-keyword = 'real estate OR investment OR mortgage rate' # sample keyword
+keyword = 'inflation OR economy' # sample keyword
 
-datascraping.get_articles(keyword)
+df = datascraping.get_articles(keyword)
+datascraping.store_to_db(df)
--- demo/datascraping.py
+++ demo/datascraping.py
@@ -3,6 +3,7 @@ import requests
 import json
 from datetime import date, timedelta
 import time
+import sqlite3
 
 # Scrape relevant articles and store
 NEWS_API = '7c0e73b941f9483bb57f879cf9f551b9'
@@ -23,9 +24,27 @@ def get_articles(keyword):
     response_json = request_response.json()
 
     articles = response_json['articles']
+    
     df = pd.DataFrame(articles)
     df = df[['title', 'author', 'description', 'url', 'publishedAt', 'content']]
 
+    # Replace content column with scraped full text
+    content = get_content(df['url'])
+    df['content'] = content
+    return df
+
 def get_content(URLs):
-    for URL in URLs:
-        scrape_bbc_articles.visit_link(URL)
+    # TODO: get full text from URL (refer to scrape_bbc_articles.py)
+    return URLs
+
+def store_to_db(df):
+    # Connect to 'data.db', a database I created for the demo. You would want to connect to our main database, init.db
+    conn = sqlite3.connect('data.db')
+
+    # Store dataframe in database
+    # Right now, we replace the database with the new dataframe
+    df.to_sql('article', conn, if_exists='replace', index=False)
+
+    # Commit and close the connection
+    conn.commit()
+    conn.close()
