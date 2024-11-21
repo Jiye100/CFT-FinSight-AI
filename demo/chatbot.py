dd8bce6932b98b9dc3bf38034a9ffd3c83d21cba
ahappyphrog <132954428+ahappyphrog@users.noreply.github.com>
2024-11-21 01:12:57 -0500

    Added requirements.txt

--- demo/chatbot.py
+++ demo/chatbot.py
@@ -12,7 +12,7 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma
 
 # Load the document, split it into chunks, embed each chunk and load it into the vector store.
-raw_documents = TextLoader('../../../state_of_the_union.txt').load()
+raw_documents = TextLoader('../../../state_of_the_union.txt').load() 
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 documents = text_splitter.split_documents(raw_documents)
 db = Chroma.from_documents(documents, OpenAIEmbeddings())
