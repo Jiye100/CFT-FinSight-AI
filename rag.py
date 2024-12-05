b09bb309cf2b270e13eee74a616c913e3c1c2949
ahappyphrog <132954428+ahappyphrog@users.noreply.github.com>
2024-12-05 14:08:43 -0500

    Implemented generate_answer and API integration

--- rag.py
+++ rag.py
@@ -15,6 +15,9 @@ from config import GEMINI_API_KEY
 embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
 embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
 
+# Config Gemini API Key
+genai.configure(api_key=GEMINI_API_KEY)
+
 # Directory to store the Chroma database
 CHROMA_PATH = "chroma"
 
@@ -27,12 +30,12 @@ def generate_keyword(query):
         str: A single string of keywords in a format of keyword1 OR keyword2 OR ...
     """
     model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(f"Here is a client's finance-related question: {user_query}. What are some main topics I could filter by to search for relevant news articles? Keep your answer short. Make sure the format is keyword1 OR keyword2 OR keyword3 ...")
+    response = model.generate_content(f"Here is a client's finance-related question: {query}. What are some main topics I could filter by to search for relevant news articles? Keep your answer short. Make sure the format is keyword1 OR keyword2 OR keyword3 ...")
 
     data = response.to_dict()
-    keyword = data['candidates'][0]['content']['parts'][0]['text']
+    keywords = data['candidates'][0]['content']['parts'][0]['text']
     
-    return keyword
+    return keywords.strip()
 
 def split_text(documents):
     """
@@ -98,7 +101,7 @@ def make_rag_prompt(query, relevant_passage):
     """
     # TODO
 
-def generate_answer(prompt):
+def generate_answer(query):
     """
     Generate a response using Gemini on the given prompt
     Args:
@@ -106,5 +109,12 @@ def generate_answer(prompt):
     Returns:
        str: A formatted response including generated text and sources 
     """
-    # TODO
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(f"{query}")
+
+    data = response.to_dict()
+    response = data['candidates'][0]['content']['parts'][0]['text']
     
+    return response.strip()
+    
+print(generate_answer("hello"))
