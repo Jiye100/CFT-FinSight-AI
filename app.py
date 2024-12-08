769de9daecc5fe91c160be8a380f7a0bd36fe1f9
Niti Goyal <go.nitigoyal@gmail.com>
2024-12-08 17:21:18 -0500

    Add progress messages

--- app.py
+++ app.py
@@ -11,30 +11,46 @@ genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 user_query = input("What can I help you with?\n")
-
 keyword = rag.generate_keyword(user_query)
+print("Generating keywords related to the question...")
+print(keyword)
 
-#Only need to run this once to fill bbc_articles.csv
+########## LOAD IN ARTICLE DATA ###########
+#If we are using a hard-coded sample dataframe of articles for a demo:
 #scrape_bbc_articles.scrape_samples()
-#df = datascraping.process_articles()
+#df = pd.read_csv("Articles/sample_bbc_articles.csv")
+
+# #If we are creating a new dataframe by searching for relevant articles with NewsAPI:
+print("Searching for relevant news articles...")
+datascraping.get_articles_using_newsapi(keyword)
+datascraping.process_articles()
+articles = pd.read_csv("articles.csv")
+#print(articles.head())
+urls=articles['url']
+#print(urls)
+
+print("Scraping news articles...")
+scrape_bbc_articles.scrape_articles(urls)
 df = pd.read_csv("Articles/bbc_articles.csv")
 
 # Convert string representations of lists to actual lists
 df['Headlines'] = df['Headlines'].apply(ast.literal_eval)
 df['Text Body'] = df['Text Body'].apply(ast.literal_eval)
-    
+
+print("Extracting relevant sections...")
 documents = df["Text Body"]
 titles = df["Title"]
 #print(documents)
-print(type(documents))
+#print(type(documents))
 chunks = rag.split_text_modified(documents)
-print("----CHUNKS------")
-print(len(chunks))
+#print("----CHUNKS------")
+#print(len(chunks))
 ids = [str(i) for i in range(len(chunks))]
-print("----DOCUMENTS------")
-print(len(documents))
-
-#print (chunks[0])
+#print("----DOCUMENTS------")
+#print(len(documents))
+print (chunks[0])
+print (chunks[1])
+print (chunks[2])
 
 #print(chunks)
 # datascraping.store_to_db(pd.read_csv('scraping/articles.csv'))
@@ -46,8 +62,8 @@ print(len(documents))
 # relevant_text = rag.query_chroma(user_query, collection)
 
 #print(documents[0])
-print("\n\n\nHI HIHIHI CHUNKS BELOW\n\n\n\n")
-print(chunks[0])
+#print("\n\n\nFIRST CHUNK BELOW\n\n\n\n")
+#print(chunks[0])
 
 import chromadb
 chroma_client = chromadb.Client()
@@ -61,18 +77,20 @@ collection.upsert(
 
 results = collection.query(
     query_texts=[user_query], # Chroma will embed this for you
-    n_results=1 # how many results to return
+    n_results=5 # how many results to return
 )
 
-print(results)
-print(type(results))
-print(type(results["documents"][0]))
+# print(results)
+# print(type(results))
+# print(type(results["documents"][0]))
 
-print("\n\n\n\nRELEVANT CONTEXT:\n\n")
-print(results["documents"][0])
+#print("\n\n\n\nRELEVANT CONTEXT:\n\n")
+#print(results["documents"][0])
 
+print("Synthesizing information to answer your question...")
 final_prompt = rag.make_rag_prompt(user_query, results["documents"][0])
 final_answer = rag.generate_answer(final_prompt)
-
-print("\n\n\n\nANSWER\n\n")
+print("\n\n\n\nPROMPT\n\n")
+print(final_prompt)
+#print("\n\n\n\nANSWER\n\n")
 print(final_answer)
